25c9de70bd12241a1241c6219f0a3396c8f28e55
unknown <tailevi455@gmail.com>
2024-12-30 15:39:40 +0200

    first commit

---
+++ src/main/java/com/example/StorePractice/StorePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.StorePractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StorePractice/StorePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StorePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
