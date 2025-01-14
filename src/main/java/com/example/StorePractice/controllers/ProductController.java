f9d72f417aa80c59403adaeedec69abe1d13d39a
unknown <tailevi455@gmail.com>
2025-01-14 11:23:39 +0200

    added db servic with operations of delete find and findAll

--- src/main/java/com/example/StorePractice/controllers/ProductController.java
+++ src/main/java/com/example/StorePractice/controllers/ProductController.java
@@ -1,13 +1,13 @@
 package com.example.StorePractice.controllers;
 
 import com.example.StorePractice.models.Product;
+import com.example.StorePractice.payload.request.ProductRequest;
+import com.example.StorePractice.payload.response.ProductResponse;
 import com.example.StorePractice.services.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,15 @@ public class ProductController {
     @GetMapping(value = "/getAll")
     public ResponseEntity<List<Product>> findAll(){
         return ResponseEntity.ok(productService.findAllProducts());
+    }
+
+    @DeleteMapping(value = "/deleteProduct")
+    public ResponseEntity<String > deleteProduct(@RequestBody ProductRequest productRequest){
+        return ResponseEntity.ok(productService.deleteProductById(productRequest.getId()));
+    }
+
+    @GetMapping(value = "/findProductById")
+    public ResponseEntity<ProductResponse> findProductById(@RequestBody ProductRequest productRequest){
+        return ResponseEntity.ok(productService.findProductById(productRequest.getId()));
     }
 }
--- src/main/java/com/example/StorePractice/services/ProductService.java
+++ src/main/java/com/example/StorePractice/services/ProductService.java
@@ -1,14 +1,18 @@
 package com.example.StorePractice.services;
 
 import com.example.StorePractice.models.Product;
+import com.example.StorePractice.payload.response.ProductResponse;
 import com.example.StorePractice.payload.response.ProductsResponse;
 import com.example.StorePractice.reposetories.ProductRepo;
 import com.example.StorePractice.reposetories.ReviewesRepo;
+import lombok.NonNull;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -20,5 +24,36 @@ public class ProductService {
 
     public List<Product> findAllProducts(){return productRepo.findAll();}
 
+    @SneakyThrows
+    public String deleteProductById(@NonNull long id){
+        productRepo.deleteById(id);
+        return "Product with the following id: "+ id +" was deleted along with his reviews";
+    }
 
+    @SneakyThrows
+    public ProductResponse findProductById(@NonNull long id){
+        Product product = productRepo.findById(id).orElseThrow(() -> new RuntimeException());
+        ProductResponse productResponse = mapProduct(product);
+        return productResponse;
+    }
+    private  ProductResponse mapProduct(Product product){
+        return ProductResponse.builder()
+                .id(product.getId())
+                .title(product.getTitle())
+                .brand(product.getBrand())
+                .price(product.getPrice())
+                .sku(product.getSku())
+                .availabilityStatus(product.getAvailabilityStatus())
+                .category(product.getCategory())
+                .description(product.getDescription())
+                .stock(product.getStock())
+                .discountPercentage(product.getDiscountPercentage())
+                .images(product.getImages())
+                .weight(product.getWeight())
+                .thumbnail(product.getThumbnail())
+                .returnPolicy(product.getReturnPolicy())
+                .warrantyInformation(product.getWarrantyInformation())
+                .shippingInformation(product.getShippingInformation())
+                .build();
+    }
 }
